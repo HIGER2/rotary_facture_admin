8803f5b8c87cf9fdc207fc1377ca8a2c886702cc
HIGER2 <doumaarmand@gmail.com>
2025-04-30 17:34:12 +0000

    moidf

--- composables/hooks/useCustomeFetch.ts
+++ composables/hooks/useCustomeFetch.ts
@@ -6,8 +6,8 @@ export async function useCustomFetch<T>(url: string | (() => string),options: Us
 
     let option = {
         ...options,
-        baseURL:'http://localhost:8000/api/',
-        // baseURL:`${config.public.apiBase}/api` || 'http://localhost:8000/api/',
+        // baseURL:'http://localhost:8000/api/',
+        baseURL:`${config.public.apiBase}/api` || 'http://localhost:8000/api/',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
